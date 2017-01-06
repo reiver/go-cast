669d92e1140bee4c4336198f604336cf2af818e6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-01-05 17:21:30 -0800

    int64

---
+++ int64.go
@@ -0,0 +1,35 @@
+package cast
+
+func Int64(v interface{}) (int64, error) {
+
+	switch value := v.(type) {
+	case uint8:
+		return int64(value), nil
+	case uint16:
+		return int64(value), nil
+	case uint32:
+		return int64(value), nil
+	case int:
+		return int64(value), nil
+	case int8:
+		return int64(value), nil
+	case int16:
+		return int64(value), nil
+	case int32:
+		return int64(value), nil
+	case int64:
+		return int64(value), nil
+	default:
+		return 0, internalCannotCastComplainer{expectedType:"int64", actualType:typeof(value)}
+	}
+}
+
+func MustInt64(v interface{}) (int64, error) {
+
+	x, err := Int64(v)
+	if nil != err {
+		panic(err)
+	}
+
+	return x, nil
+}
---
+++ int64_test.go
@@ -0,0 +1,381 @@
+package cast
+
+import (
+	"math"
+
+	"testing"
+)
+
+func TestInt64FromInt8(t *testing.T) {
+
+	tests := []struct{
+		Value int8
+	}{
+		{
+			Value: math.MinInt8,
+		},
+		{
+			Value: -1,
+		},
+		{
+			Value: 0,
+		},
+		{
+			Value: 1,
+		},
+		{
+			Value: math.MaxInt8,
+		},
+	}
+
+	{
+		const numRand = 20
+		for i:=0; i<numRand; i++ {
+			test := struct{
+				Value int8
+			}{
+				Value: int8(randomness.Int63n(math.MaxInt8)),
+			}
+			tests = append(tests, test)
+
+			test = struct{
+				Value int8
+			}{
+				Value: -int8(randomness.Int63n(-1*math.MinInt8)),
+			}
+			tests = append(tests, test)
+		}
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Int64(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := int8(x)
+
+		if expected, actual := test.Value, y; expected != actual {
+			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
+
+func TestInt64FromInt16(t *testing.T) {
+
+	tests := []struct{
+		Value int16
+	}{
+		{
+			Value: math.MinInt16,
+		},
+		{
+			Value: -1,
+		},
+		{
+			Value: 0,
+		},
+		{
+			Value: 1,
+		},
+		{
+			Value: math.MaxInt16,
+		},
+	}
+
+	{
+		const numRand = 20
+		for i:=0; i<numRand; i++ {
+			test := struct{
+				Value int16
+			}{
+				Value: int16(randomness.Int63n(math.MaxInt16)),
+			}
+			tests = append(tests, test)
+
+			test = struct{
+				Value int16
+			}{
+				Value: -int16(randomness.Int63n(-1*math.MinInt16)),
+			}
+			tests = append(tests, test)
+		}
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Int64(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := int16(x)
+
+		if expected, actual := test.Value, y; expected != actual {
+			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
+
+func TestInt64FromInt32(t *testing.T) {
+
+	tests := []struct{
+		Value int32
+	}{
+		{
+			Value: math.MinInt32,
+		},
+		{
+			Value: -1,
+		},
+		{
+			Value: 0,
+		},
+		{
+			Value: 1,
+		},
+		{
+			Value: math.MaxInt32,
+		},
+	}
+
+	{
+		const numRand = 20
+		for i:=0; i<numRand; i++ {
+			test := struct{
+				Value int32
+			}{
+				Value: int32(randomness.Int63n(math.MaxInt32)),
+			}
+			tests = append(tests, test)
+
+			test = struct{
+				Value int32
+			}{
+				Value: -int32(randomness.Int63n(-1*math.MinInt32)),
+			}
+			tests = append(tests, test)
+		}
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Int64(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := int32(x)
+
+		if expected, actual := test.Value, y; expected != actual {
+			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
+
+func TestInt64FromInt64(t *testing.T) {
+
+	tests := []struct{
+		Value int64
+	}{
+		{
+			Value: math.MinInt64,
+		},
+		{
+			Value: -1,
+		},
+		{
+			Value: 0,
+		},
+		{
+			Value: 1,
+		},
+		{
+			Value: math.MaxInt64,
+		},
+	}
+
+	{
+		const numRand = 20
+		for i:=0; i<numRand; i++ {
+			test := struct{
+				Value int64
+			}{
+				Value: int64(randomness.Int63()),
+			}
+			tests = append(tests, test)
+
+			test = struct{
+				Value int64
+			}{
+				Value: -int64(randomness.Int63()),
+			}
+			tests = append(tests, test)
+		}
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Int64(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := int64(x)
+
+		if expected, actual := test.Value, y; expected != actual {
+			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
+
+func TestInt64FromUint8(t *testing.T) {
+
+	tests := []struct{
+		Value uint8
+	}{
+		{
+			Value: 0,
+		},
+		{
+			Value: 1,
+		},
+		{
+			Value: math.MaxUint8,
+		},
+	}
+
+	{
+		const numRand = 20
+		for i:=0; i<numRand; i++ {
+			test := struct{
+				Value uint8
+			}{
+				Value: uint8(randomness.Int63n(math.MaxUint8)),
+			}
+			tests = append(tests, test)
+		}
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Int64(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := uint8(x)
+
+		if expected, actual := test.Value, y; expected != actual {
+			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
+
+func TestInt64FromUint16(t *testing.T) {
+
+	tests := []struct{
+		Value uint16
+	}{
+		{
+			Value: 0,
+		},
+		{
+			Value: 1,
+		},
+		{
+			Value: math.MaxUint16,
+		},
+	}
+
+	{
+		const numRand = 20
+		for i:=0; i<numRand; i++ {
+			test := struct{
+				Value uint16
+			}{
+				Value: uint16(randomness.Int63n(math.MaxUint16)),
+			}
+			tests = append(tests, test)
+		}
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Int64(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := uint16(x)
+
+		if expected, actual := test.Value, y; expected != actual {
+			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
+
+func TestInt64FromUint32(t *testing.T) {
+
+	tests := []struct{
+		Value uint32
+	}{
+		{
+			Value: 0,
+		},
+		{
+			Value: 1,
+		},
+		{
+			Value: math.MaxUint32,
+		},
+	}
+
+	{
+		const numRand = 20
+		for i:=0; i<numRand; i++ {
+			test := struct{
+				Value uint32
+			}{
+				Value: uint32(randomness.Int63n(math.MaxUint32)),
+			}
+			tests = append(tests, test)
+		}
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Int64(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := uint32(x)
+
+		if expected, actual := test.Value, y; expected != actual {
+			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
