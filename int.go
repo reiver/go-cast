21c9df9533a5d8d7462b95fcdc98ae36c2ab103d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-01-05 16:50:48 -0800

    int

---
+++ int.go
@@ -0,0 +1,31 @@
+package cast
+
+func Int(v interface{}) (int, error) {
+
+	switch value := v.(type) {
+	case uint8:
+		return int(value), nil
+	case uint16:
+		return int(value), nil
+	case int:
+		return int(value), nil
+	case int8:
+		return int(value), nil
+	case int16:
+		return int(value), nil
+	case int32:
+		return int(value), nil
+	default:
+		return 0, internalCannotCastComplainer{expectedType:"int", actualType:typeof(value)}
+	}
+}
+
+func MustInt(v interface{}) (int, error) {
+
+	x, err := Int(v)
+	if nil != err {
+		panic(err)
+	}
+
+	return x, nil
+}
