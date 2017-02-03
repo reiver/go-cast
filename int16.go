88f76b0d8ce191a8796e2e7a7059405b50f29652
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-02-03 14:20:34 -0800

    improved cast.Int16()

--- int16.go
+++ int16.go
@@ -1,16 +1,51 @@
 package cast
 
+// Int16 will return an int16 when `v` is of type int16, int8, uint8 or has a method:
+//
+//	type interface {
+//		Int16() (int16, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Int8() (int8, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Uint8() (uint8, error)
+//	}
+//
+// Else it will return an error.
 func Int16(v interface{}) (int16, error) {
 
 	switch value := v.(type) {
-	case uint8:
-		return int16(value), nil
-	case int8:
-		return int16(value), nil
 	case int16:
 		return int16(value), nil
 	case int16er:
 		return value.Int16()
+	case int8:
+		return int16(value), nil
+	case int8er:
+		return func()(int16, error){
+			value, err := value.Int8()
+			if nil != err {
+				return 0, err
+			}
+			return int16(value), nil
+		}()
+	case uint8:
+		return int16(value), nil
+	case uint8er:
+		return func()(int16, error){
+			value, err := value.Uint8()
+			if nil != err {
+				return 0, err
+			}
+			return int16(value), nil
+		}()
 	default:
 		return 0, internalCannotCastComplainer{expectedType:"int16", actualType:typeof(value)}
 	}
