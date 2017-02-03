deff236d8cae5380e3cd4701c596731d5595dc2e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-02-03 15:56:20 -0800

    added more docs. and improved cast.Uint64()

--- uint64.go
+++ uint64.go
@@ -1,20 +1,85 @@
 package cast
 
+// Uint64 will return an int64 when `v` is of type uint64, uint32, uint16, uint8, uint or has a method:
+//
+//	type interface {
+//		Uint64() (uint64, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Uint32() (uint32, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Uint16() (uint16, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Uint8() (uint8, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Uint() (uint, error)
+//	}
+//
+// ... that returns successfully.
+//
+// Else it will return an error.
 func Uint64(v interface{}) (uint64, error) {
 
 	switch value := v.(type) {
-	case uint:
-		return uint64(value), nil
-	case uint8:
-		return uint64(value), nil
-	case uint16:
-		return uint64(value), nil
-	case uint32:
-		return uint64(value), nil
 	case uint64:
 		return uint64(value), nil
 	case uint64er:
 		return value.Uint64()
+	case uint32:
+		return uint64(value), nil
+	case uint32er:
+		return func()(uint64, error){
+			casted, err := value.Uint32()
+			if nil != err {
+				return 0, err
+			}
+			return uint64(casted), nil
+		}()
+	case uint:
+		return uint64(value), nil
+	case uinter:
+		return func()(uint64, error) {
+			casted, err := value.Uint()
+			if nil != err {
+				return 0, err
+			}
+			return uint64(casted), nil
+		}()
+	case uint16:
+		return uint64(value), nil
+	case uint16er:
+		return func()(uint64, error){
+			casted, err := value.Uint16()
+			if nil != err {
+				return 0, err
+			}
+			return uint64(casted), nil
+		}()
+	case uint8:
+		return uint64(value), nil
+	case uint8er:
+		return func()(uint64, error){
+			casted, err := value.Uint8()
+			if nil != err {
+				return 0, err
+			}
+			return uint64(casted), nil
+		}()
 	default:
 		return 0, internalCannotCastComplainer{expectedType:"uint64", actualType:typeof(value)}
 	}
