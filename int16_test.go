d6b0077d190a143d83494adfe16ad6397cb21285
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-02-03 14:26:27 -0800

    cosmetic change. most some tests in file.

--- int16_test.go
+++ int16_test.go
@@ -6,65 +6,6 @@ import (
 	"testing"
 )
 
-func TestInt16FromInt8(t *testing.T) {
-
-	tests := []struct{
-		Value int8
-	}{
-		{
-			Value: math.MinInt8,
-		},
-		{
-			Value: -1,
-		},
-		{
-			Value: 0,
-		},
-		{
-			Value: 1,
-		},
-		{
-			Value: math.MaxInt8,
-		},
-	}
-
-	{
-		const numRand = 20
-		for i:=0; i<numRand; i++ {
-			test := struct{
-				Value int8
-			}{
-				Value: int8(randomness.Int63n(math.MaxInt8)),
-			}
-			tests = append(tests, test)
-
-			test = struct{
-				Value int8
-			}{
-				Value: -int8(randomness.Int63n(-1*math.MinInt8)),
-			}
-			tests = append(tests, test)
-		}
-	}
-
-
-	for testNumber, test := range tests {
-
-		x, err := Int16(test.Value)
-		if nil != err {
-			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
-			continue
-		}
-
-		y := int8(x)
-
-		if expected, actual := test.Value, y; expected != actual {
-			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
-			continue
-		}
-	}
-}
-
 func TestInt16FromInt16(t *testing.T) {
 
 	tests := []struct{
@@ -170,6 +111,65 @@ func TestInt16FromInt16er(t *testing.T) {
 	}
 }
 
+
+func TestInt16FromInt8(t *testing.T) {
+
+	tests := []struct{
+		Value int8
+	}{
+		{
+			Value: math.MinInt8,
+		},
+		{
+			Value: -1,
+		},
+		{
+			Value: 0,
+		},
+		{
+			Value: 1,
+		},
+		{
+			Value: math.MaxInt8,
+		},
+	}
+
+	{
+		const numRand = 20
+		for i:=0; i<numRand; i++ {
+			test := struct{
+				Value int8
+			}{
+				Value: int8(randomness.Int63n(math.MaxInt8)),
+			}
+			tests = append(tests, test)
+
+			test = struct{
+				Value int8
+			}{
+				Value: -int8(randomness.Int63n(-1*math.MinInt8)),
+			}
+			tests = append(tests, test)
+		}
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Int16(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := int8(x)
+
+		if expected, actual := test.Value, y; expected != actual {
+			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
 func TestInt16FromUint8(t *testing.T) {
 
 	tests := []struct{
