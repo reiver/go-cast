f8449d477b9d9e8ba3c7b77e66f1ffa49a282c95
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-02-03 15:33:41 -0800

    added more docs. and improved cast.Uint32()

--- uint32.go
+++ uint32.go
@@ -1,16 +1,53 @@
 package cast
 
+// Uint32 will return an int32 when `v` is of type uint32, uint16, uint8 or has a method:
+//
+//	type interface {
+//		Uint32() (uint32, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Uint16() (uint16, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Uint8() (uint8, error)
+//	}
+//
+// ... that returns successfully.
+//
+// Else it will return an error.
 func Uint32(v interface{}) (uint32, error) {
 
 	switch value := v.(type) {
-	case uint8:
-		return uint32(value), nil
-	case uint16:
-		return uint32(value), nil
 	case uint32:
 		return uint32(value), nil
 	case uint32er:
 		return value.Uint32()
+	case uint16:
+		return uint32(value), nil
+	case uint16er:
+		return func()(uint32, error){
+			casted, err := value.Uint16()
+			if nil != err {
+				return 0, err
+			}
+			return uint32(casted), nil
+		}()
+	case uint8:
+		return uint32(value), nil
+	case uint8er:
+		return func()(uint32, error){
+			casted, err := value.Uint8()
+			if nil != err {
+				return 0, err
+			}
+			return uint32(casted), nil
+		}()
 	default:
 		return 0, internalCannotCastComplainer{expectedType:"uint32", actualType:typeof(value)}
 	}
@@ -29,4 +66,5 @@ func MustUint32(v interface{}) uint32 {
 
 type uint32er interface {
 	Uint32() (uint32, error)
+
 }
