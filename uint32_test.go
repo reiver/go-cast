a9d6c2951ff24582ee7ad4521672cb12ab783ef5
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-02-03 15:39:41 -0800

    added more tests

--- uint32_test.go
+++ uint32_test.go
@@ -136,6 +136,44 @@ func TestUint32FromUint16(t *testing.T) {
 	}
 }
 
+func TestUint32FromUint16er(t *testing.T) {
+
+	tests := []struct{
+		Value    uint16er
+		Expected uint16
+	}{
+		{
+			Value: testUint16erZero(),
+			Expected:         0,
+		},
+		{
+			Value: testUint16erOne(),
+			Expected:         1,
+		},
+		{
+			Value: testUint16erMax(),
+			Expected:    math.MaxUint16,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Uint32(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := uint16(x)
+
+		if expected, actual := test.Expected, y; expected != actual {
+			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
+
 func TestUint32FromUint8(t *testing.T) {
 
 	tests := []struct{
