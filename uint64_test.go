c7b759b9ec5afe21297bf1cf3349cfd0b2432223
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-02-03 16:08:31 -0800

    added more tests

--- uint64_test.go
+++ uint64_test.go
@@ -303,3 +303,41 @@ func TestUint64FromUint8(t *testing.T) {
 		}
 	}
 }
+
+func TestUint64FromUint8er(t *testing.T) {
+
+	tests := []struct{
+		Value    uint8er
+		Expected uint8
+	}{
+		{
+			Value: testUint8erZero(),
+			Expected:         0,
+		},
+		{
+			Value: testUint8erOne(),
+			Expected:         1,
+		},
+		{
+			Value: testUint8erMax(),
+			Expected:    math.MaxUint8,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Uint64(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := uint8(x)
+
+		if expected, actual := test.Expected, y; expected != actual {
+			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
