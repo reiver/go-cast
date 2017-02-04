42f61c9458baed3451e68e9d0032521e89143250
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-02-03 18:22:45 -0800

    added more docs. and improved cast.Float32()

--- float32.go
+++ float32.go
@@ -1,19 +1,84 @@
 package cast
 
+// Float32 will return an float32 when `v` is of type float32, int16, int8, uint16, uint8 or has a method:
+//
+//	type interface {
+//		Float32() (float32, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Int16() (int16, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Int8() (int8, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Uint16() (uint16, error)
+//	}
+//
+// ... that returns successfully, or has a method:
+//
+//	type interface {
+//		Uint8() (uint8, error)
+//	}
+//
+// ... that returns successfully.
+//
+// Else it will return an error.
 func Float32(v interface{}) (float32, error) {
 
 	switch value := v.(type) {
 	case float32er:
 		return value.Float32()
+	case int16er:
+		return func()(float32, error){
+			casted, err := value.Int16()
+			if nil != err {
+				return 0, err
+			}
+			return float32(casted), nil
+		}()
+	case int8er:
+		return func()(float32, error){
+			casted, err := value.Int8()
+			if nil != err {
+				return 0, err
+			}
+			return float32(casted), nil
+		}()
+	case uint16er:
+		return func()(float32, error){
+			casted, err := value.Uint16()
+			if nil != err {
+				return 0, err
+			}
+			return float32(casted), nil
+		}()
+	case uint8er:
+		return func()(float32, error){
+			casted, err := value.Uint8()
+			if nil != err {
+				return 0, err
+			}
+			return float32(casted), nil
+		}()
 	case float32:
 		return float32(value), nil
-	case uint8:
-		return float32(value), nil
-	case uint16:
+	case int16:
 		return float32(value), nil
 	case int8:
 		return float32(value), nil
-	case int16:
+	case uint16:
+		return float32(value), nil
+	case uint8:
 		return float32(value), nil
 	default:
 		return 0, internalCannotCastComplainer{expectedType:"float32", actualType:typeof(value)}
