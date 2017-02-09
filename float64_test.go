80ef4bc19c70b3a1721d9fd0782ba5aae5c51053
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-02-09 15:12:29 -0800

    cosmetic code change. changes order of tests in file

--- float64_test.go
+++ float64_test.go
@@ -6,141 +6,6 @@ import (
 	"testing"
 )
 
-func TestFloat64FromFloat32(t *testing.T) {
-
-	tests := []struct{
-		Value float32
-	}{
-		{
-			Value: float32(math.Inf(-1)),
-		},
-		{
-			Value: -math.MaxFloat32,
-		},
-		{
-			Value: -math.Pi,
-		},
-		{
-			Value: -math.E,
-		},
-		{
-			Value: -math.Sqrt2,
-		},
-		{
-			Value: -1.0,
-		},
-		{
-			Value: -math.Ln2,
-		},
-		{
-			Value: -math.SmallestNonzeroFloat32,
-		},
-		{
-			Value: 0.0,
-		},
-		{
-			Value: math.SmallestNonzeroFloat32,
-		},
-		{
-			Value: math.Ln2,
-		},
-		{
-			Value: 1.0,
-		},
-		{
-			Value: math.Sqrt2,
-		},
-		{
-			Value: math.E,
-		},
-		{
-			Value: math.Pi,
-		},
-		{
-			Value: math.MaxFloat32,
-		},
-		{
-			Value: float32(math.Inf(+1)),
-		},
-
-
-
-		{
-			Value: float32(math.NaN()),
-		},
-	}
-
-	{
-		const numRand = 20
-		for i:=0; i<numRand; i++ {
-			test := struct{
-				Value float32
-			}{
-				Value: randomness.Float32(),
-			}
-			tests = append(tests, test)
-
-			test = struct{
-				Value float32
-			}{
-				Value: -randomness.Float32(),
-			}
-			tests = append(tests, test)
-
-
-
-			test = struct{
-				Value float32
-			}{
-				Value: randomness.Float32() * math.MaxFloat32,
-			}
-			tests = append(tests, test)
-
-			test = struct{
-				Value float32
-			}{
-				Value: -randomness.Float32() * math.MaxFloat32,
-			}
-			tests = append(tests, test)
-
-
-
-			test = struct{
-				Value float32
-			}{
-				Value: randomness.Float32() * 999999999,
-			}
-			tests = append(tests, test)
-
-			test = struct{
-				Value float32
-			}{
-				Value: -randomness.Float32() * 999999999,
-			}
-			tests = append(tests, test)
-		}
-	}
-
-
-	for testNumber, test := range tests {
-
-		x, err := Float64(test.Value)
-		if nil != err {
-			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
-			continue
-		}
-
-		y := float32(x)
-
-		if expected, actual := test.Value, y; expected != actual {
-			if !(math.IsNaN(float64(expected)) && math.IsNaN(float64(actual))) {
-				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
-				continue
-			}
-		}
-	}
-}
-
 func TestFloat64FromFloat64(t *testing.T) {
 
 	tests := []struct{
@@ -276,25 +141,67 @@ func TestFloat64FromFloat64(t *testing.T) {
 	}
 }
 
-func TestFloat64FromInt8(t *testing.T) {
+func TestFloat64FromFloat32(t *testing.T) {
 
 	tests := []struct{
-		Value int8
+		Value float32
 	}{
 		{
-			Value: math.MinInt8,
+			Value: float32(math.Inf(-1)),
 		},
 		{
-			Value: -1,
+			Value: -math.MaxFloat32,
 		},
 		{
-			Value: 0,
+			Value: -math.Pi,
 		},
 		{
-			Value: 1,
+			Value: -math.E,
 		},
 		{
-			Value: math.MaxInt8,
+			Value: -math.Sqrt2,
+		},
+		{
+			Value: -1.0,
+		},
+		{
+			Value: -math.Ln2,
+		},
+		{
+			Value: -math.SmallestNonzeroFloat32,
+		},
+		{
+			Value: 0.0,
+		},
+		{
+			Value: math.SmallestNonzeroFloat32,
+		},
+		{
+			Value: math.Ln2,
+		},
+		{
+			Value: 1.0,
+		},
+		{
+			Value: math.Sqrt2,
+		},
+		{
+			Value: math.E,
+		},
+		{
+			Value: math.Pi,
+		},
+		{
+			Value: math.MaxFloat32,
+		},
+		{
+			Value: float32(math.Inf(+1)),
+		},
+
+
+
+		{
+			Value: float32(math.NaN()),
 		},
 	}
 
@@ -302,16 +209,48 @@ func TestFloat64FromInt8(t *testing.T) {
 		const numRand = 20
 		for i:=0; i<numRand; i++ {
 			test := struct{
-				Value int8
+				Value float32
 			}{
-				Value: int8(randomness.Int63n(math.MaxInt8)),
+				Value: randomness.Float32(),
 			}
 			tests = append(tests, test)
 
 			test = struct{
-				Value int8
+				Value float32
 			}{
-				Value: -int8(randomness.Int63n(-1*math.MinInt8)),
+				Value: -randomness.Float32(),
+			}
+			tests = append(tests, test)
+
+
+
+			test = struct{
+				Value float32
+			}{
+				Value: randomness.Float32() * math.MaxFloat32,
+			}
+			tests = append(tests, test)
+
+			test = struct{
+				Value float32
+			}{
+				Value: -randomness.Float32() * math.MaxFloat32,
+			}
+			tests = append(tests, test)
+
+
+
+			test = struct{
+				Value float32
+			}{
+				Value: randomness.Float32() * 999999999,
+			}
+			tests = append(tests, test)
+
+			test = struct{
+				Value float32
+			}{
+				Value: -randomness.Float32() * 999999999,
 			}
 			tests = append(tests, test)
 		}
@@ -326,68 +265,7 @@ func TestFloat64FromInt8(t *testing.T) {
 			continue
 		}
 
-		y := int8(x)
-
-		if expected, actual := test.Value, y; expected != actual {
-			if !(math.IsNaN(float64(expected)) && math.IsNaN(float64(actual))) {
-				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
-				continue
-			}
-		}
-	}
-}
-
-func TestFloat64FromInt16(t *testing.T) {
-
-	tests := []struct{
-		Value int16
-	}{
-		{
-			Value: math.MinInt16,
-		},
-		{
-			Value: -1,
-		},
-		{
-			Value: 0,
-		},
-		{
-			Value: 1,
-		},
-		{
-			Value: math.MaxInt16,
-		},
-	}
-
-	{
-		const numRand = 20
-		for i:=0; i<numRand; i++ {
-			test := struct{
-				Value int16
-			}{
-				Value: int16(randomness.Int63n(math.MaxInt16)),
-			}
-			tests = append(tests, test)
-
-			test = struct{
-				Value int16
-			}{
-				Value: -int16(randomness.Int63n(-1*math.MinInt16)),
-			}
-			tests = append(tests, test)
-		}
-	}
-
-
-	for testNumber, test := range tests {
-
-		x, err := Float64(test.Value)
-		if nil != err {
-			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
-			continue
-		}
-
-		y := int16(x)
+		y := float32(x)
 
 		if expected, actual := test.Value, y; expected != actual {
 			if !(math.IsNaN(float64(expected)) && math.IsNaN(float64(actual))) {
@@ -459,11 +337,17 @@ func TestFloat64FromInt32(t *testing.T) {
 	}
 }
 
-func TestFloat64FromUint8(t *testing.T) {
+func TestFloat64FromInt16(t *testing.T) {
 
 	tests := []struct{
-		Value uint8
+		Value int16
 	}{
+		{
+			Value: math.MinInt16,
+		},
+		{
+			Value: -1,
+		},
 		{
 			Value: 0,
 		},
@@ -471,7 +355,7 @@ func TestFloat64FromUint8(t *testing.T) {
 			Value: 1,
 		},
 		{
-			Value: math.MaxUint8,
+			Value: math.MaxInt16,
 		},
 	}
 
@@ -479,9 +363,16 @@ func TestFloat64FromUint8(t *testing.T) {
 		const numRand = 20
 		for i:=0; i<numRand; i++ {
 			test := struct{
-				Value uint8
+				Value int16
 			}{
-				Value: uint8(randomness.Int63n(math.MaxUint8)),
+				Value: int16(randomness.Int63n(math.MaxInt16)),
+			}
+			tests = append(tests, test)
+
+			test = struct{
+				Value int16
+			}{
+				Value: -int16(randomness.Int63n(-1*math.MinInt16)),
 			}
 			tests = append(tests, test)
 		}
@@ -496,7 +387,116 @@ func TestFloat64FromUint8(t *testing.T) {
 			continue
 		}
 
-		y := uint8(x)
+		y := int16(x)
+
+		if expected, actual := test.Value, y; expected != actual {
+			if !(math.IsNaN(float64(expected)) && math.IsNaN(float64(actual))) {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestFloat64FromInt8(t *testing.T) {
+
+	tests := []struct{
+		Value int8
+	}{
+		{
+			Value: math.MinInt8,
+		},
+		{
+			Value: -1,
+		},
+		{
+			Value: 0,
+		},
+		{
+			Value: 1,
+		},
+		{
+			Value: math.MaxInt8,
+		},
+	}
+
+	{
+		const numRand = 20
+		for i:=0; i<numRand; i++ {
+			test := struct{
+				Value int8
+			}{
+				Value: int8(randomness.Int63n(math.MaxInt8)),
+			}
+			tests = append(tests, test)
+
+			test = struct{
+				Value int8
+			}{
+				Value: -int8(randomness.Int63n(-1*math.MinInt8)),
+			}
+			tests = append(tests, test)
+		}
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Float64(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := int8(x)
+
+		if expected, actual := test.Value, y; expected != actual {
+			if !(math.IsNaN(float64(expected)) && math.IsNaN(float64(actual))) {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestFloat64FromUint32(t *testing.T) {
+
+	tests := []struct{
+		Value uint32
+	}{
+		{
+			Value: 0,
+		},
+		{
+			Value: 1,
+		},
+		{
+			Value: math.MaxUint32,
+		},
+	}
+
+	{
+		const numRand = 20
+		for i:=0; i<numRand; i++ {
+			test := struct{
+				Value uint32
+			}{
+				Value: uint32(randomness.Int63n(math.MaxUint32)),
+			}
+			tests = append(tests, test)
+		}
+	}
+
+
+	for testNumber, test := range tests {
+
+		x, err := Float64(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := uint32(x)
 
 		if expected, actual := test.Value, y; expected != actual {
 			if !(math.IsNaN(float64(expected)) && math.IsNaN(float64(actual))) {
@@ -555,10 +555,10 @@ func TestFloat64FromUint16(t *testing.T) {
 	}
 }
 
-func TestFloat64FromUint32(t *testing.T) {
+func TestFloat64FromUint8(t *testing.T) {
 
 	tests := []struct{
-		Value uint32
+		Value uint8
 	}{
 		{
 			Value: 0,
@@ -567,7 +567,7 @@ func TestFloat64FromUint32(t *testing.T) {
 			Value: 1,
 		},
 		{
-			Value: math.MaxUint32,
+			Value: math.MaxUint8,
 		},
 	}
 
@@ -575,9 +575,9 @@ func TestFloat64FromUint32(t *testing.T) {
 		const numRand = 20
 		for i:=0; i<numRand; i++ {
 			test := struct{
-				Value uint32
+				Value uint8
 			}{
-				Value: uint32(randomness.Int63n(math.MaxUint32)),
+				Value: uint8(randomness.Int63n(math.MaxUint8)),
 			}
 			tests = append(tests, test)
 		}
@@ -592,7 +592,7 @@ func TestFloat64FromUint32(t *testing.T) {
 			continue
 		}
 
-		y := uint32(x)
+		y := uint8(x)
 
 		if expected, actual := test.Value, y; expected != actual {
 			if !(math.IsNaN(float64(expected)) && math.IsNaN(float64(actual))) {
