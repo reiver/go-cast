b247b83651766deae566125cf5c99536b1811a77
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-02-09 15:18:16 -0800

    moved test into its own file

--- float64_test.go
+++ float64_test.go
@@ -6,67 +6,6 @@ import (
 	"testing"
 )
 
-func TestFloat64FromInt32(t *testing.T) {
-
-	tests := []struct{
-		Value int32
-	}{
-		{
-			Value: math.MinInt32,
-		},
-		{
-			Value: -1,
-		},
-		{
-			Value: 0,
-		},
-		{
-			Value: 1,
-		},
-		{
-			Value: math.MaxInt32,
-		},
-	}
-
-	{
-		const numRand = 20
-		for i:=0; i<numRand; i++ {
-			test := struct{
-				Value int32
-			}{
-				Value: int32(randomness.Int63n(math.MaxInt32)),
-			}
-			tests = append(tests, test)
-
-			test = struct{
-				Value int32
-			}{
-				Value: -int32(randomness.Int63n(-1*math.MinInt32)),
-			}
-			tests = append(tests, test)
-		}
-	}
-
-
-	for testNumber, test := range tests {
-
-		x, err := Float64(test.Value)
-		if nil != err {
-			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
-			continue
-		}
-
-		y := int32(x)
-
-		if expected, actual := test.Value, y; expected != actual {
-			if !(math.IsNaN(float64(expected)) && math.IsNaN(float64(actual))) {
-				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
-				continue
-			}
-		}
-	}
-}
-
 func TestFloat64FromInt16(t *testing.T) {
 
 	tests := []struct{
